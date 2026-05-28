5d145478b99696a14f4a5304eaa0d1794bc6ef5b
Cicero <cicerosilvaalmidajunior@gmail.com>
2026-05-27 21:33:36 -0300

    Ârquitetura MVC adicionada no nosso projeto

--- tele-severino-flask/app/services/review_service.py
+++
@@ -1,8 +0,0 @@
-from app.repositories.review_repository import create_review
-
-
-def save_review(user_id, specialist_id, rating, comment, call_id=1):
-    # Regra futura:
-    # - Permitir avaliação apenas após chamada finalizada.
-    # - Permitir avaliação apenas após pagamento confirmado.
-    return create_review(user_id, specialist_id, call_id, rating, comment)
