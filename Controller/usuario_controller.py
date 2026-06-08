18acc4eb9d3c4b557de63519ee2b42f767d9bc82
Cicero <cicerosilvaalmidajunior@gmail.com>
2026-06-08 01:17:06 -0300

    Foi criado um end-point e implementado a função do hash

--- Controller/usuario_controller.py
+++ Controller/usuario_controller.py
@@ -1,15 +1,25 @@
 from fastapi import APIRouter
 from Model.usuario_model import cadastrar_usuario
+from Model.usuario_model import consultar_usuarios
 
 router = APIRouter()
 
 @router.post("/usuarios")
 def criar_usuario(dados: dict = None):
-    nome = "Cicero Jegue" #dados.get("nome")
-    email = "ciceroExplodeBunda@gmail.com" #dados.get("email")
+    nome = "Cicero" #dados.get("nome")
+    email = "cicero@gmail.com" #dados.get("email")
     senha = "1234"  #dados.get("senha")
     tipo = "cliente" #dados.get("tipo")
 
     resultado = cadastrar_usuario(nome, email, senha, tipo)
 
     return resultado
+
+@router.post("/usuarios/login")
+def consulta_usuario(email: str):
+    usuario = consultar_usuarios(email)
+
+    return {"mensagem": f"Dados retornado {usuario}"}
+
+
+consulta_usuario("cicero@gmail.com")
--- Model/usuario_model.py
+++ Model/usuario_model.py
@@ -1,4 +1,14 @@
 from Model.database import conectar_banco
+import hashlib
+
+def senha_hash(senha):
+    texto = str(senha).encode()
+
+    hash_objeto = hashlib.sha3_256(texto)
+
+    codigo_hash = hash_objeto.hexdigest()
+
+    return codigo_hash
 
 def cadastrar_usuario(nome, email, senha, tipo):
     try:
@@ -10,7 +20,7 @@ def cadastrar_usuario(nome, email, senha, tipo):
         VALUES (%s, %s, %s, %s)
         """
 
-        valores = (nome, email, senha, tipo)
+        valores = (nome, email, senha_hash(senha), tipo)
 
         cursor.execute(sql, valores)
         conexao.commit()
@@ -20,6 +30,27 @@ def cadastrar_usuario(nome, email, senha, tipo):
     except Exception as erro:
         return {"erro": str(erro)}
 
+    finally:
+        if conexao:
+            conexao.close()
+
+def consultar_usuarios(email):
+    try:
+        conexao = conectar_banco()
+        cursor = conexao.cursor()
+
+        sql = """
+        SELECT id_usuario, nome, email, senha, tipo FROM usuarios WHERE email = %s
+        """
+        cursor.execute(sql,(email,))
+
+        usuario = cursor.fetchone()
+
+        return usuario
+    
+    except Exception as erro:
+        return {"erro": str(erro)}
+        
     finally:
         if conexao:
             conexao.close()
